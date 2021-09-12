f920109703e3c104df2f9a009680dc8aaad3982b
Frankiness <690626310@qq.com>
2021-09-12 18:20:12 +0800

    feat:删除多余文件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./views/Home.vue";
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
+<template><Home></Home></template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  border-radius: 10rem;
+  background-color: rgb(150, 150, 150);
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><h4>this is home page</h4></div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
